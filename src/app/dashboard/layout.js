b248c1f02ed43048d245ee0c555090f6daa05665
Rocky Haque <rockyhaque99@gmail.com>
2024-11-29 11:29:23 +0600

    tailwind config & dashbord access issue resolved

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,1 +1,17 @@
-// layout is here
+
+import { Roboto } from "next/font/google";
+import AuthProvider from "@/services/AuthProvider";
+
+const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
+
+export const metadata = {
+  title: "JourneyTrails | Dashboard",
+};
+
+export default function DashboardRootLayout({ children }) {
+  return (
+    <div className={`${roboto.className}`}>
+      <AuthProvider>{children}</AuthProvider>
+    </div>
+  );
+}
