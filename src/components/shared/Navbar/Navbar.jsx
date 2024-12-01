5e1a6589e6ab7733ac90bf72042f8e73d85a1339
iffarurrahat <iffarurrahat@gmail.com>
2024-12-02 04:19:08 +0600

    Login page design done

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import NavTitle from "@/components/shared/Navbar/NavTitle";
 
 const Navbar = () => {
   return (
-    <nav className="bg-background backdrop-blur-lg bg-opacity-75 py-3 top-0 z-50 shadow-bottom-shadow">
+    <nav className="bg-background sticky backdrop-blur-lg bg-opacity-75 py-3 top-0 z-50 shadow-bottom-shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           {/* Nav Title */}
